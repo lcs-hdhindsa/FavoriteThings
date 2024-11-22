8a62ff8161df56c7f13e4a052c2848ed805702d4
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2024-11-22 08:43:40 -0500

    Initial Commit

---
+++ FavoriteThings/FavoriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsApp.swift
+//  FavoriteThings
+//
+//  Created by Harshan Dhindsa on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
